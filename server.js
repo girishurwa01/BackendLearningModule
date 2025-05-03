5a55e150cf0c94252cd5e4048b4bfb5b19b68f82
girishurwa <girishurwa@gmail.com>
2025-05-03 21:29:12 +0530

    � First express server with simple routes

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//Route1 Home
+
+app.get('/',(req,res)=>{
+    res.send("Girish has just started learning backend 03-05-2025")
+});
+
+
+//Excercise 1 with username from url
+app.get('/hello/:name',(req,res)=>{
+    const username = req.params.name;
+    res.send(`First backend code learnt ${username}`)
+})
+
+//add 2 numbers from url
+app.get('/home/:a/:b',(req,res)=>{
+    const a =parseInt(req.params.a);
+    const b =parseInt(req.params.b);
+    res.json(
+        {
+            sum:a+b
+        }
+    );
+});
+
+
+app.listen(port, ()=>{
+    console.log(`My first handmade server is running now on ${port}`);
+});
+
